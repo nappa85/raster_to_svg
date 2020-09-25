69ee13c4179b9f4c135dffbac7ffb70bad8fbf82
Marco Napetti <mnapetti@alpenite.com>
2020-09-25 11:27:00 +0200

    parametric dimensions

--- convert.sh
+++ convert.sh
@@ -2,6 +2,8 @@
 GIF=${1/.png/.gif};
 SVG=${1/.png/.svg};
 echo "$1 -> $SVG";
+W=`identify -format "%[fx:w]" $1`;
+H=`identify -format "%[fx:h]" $1`;
 # convert to gif with 64 indexed colors, trying to eliminate dust spots
 convert $1 -colors 64 -morphology open disk:1 $GIF;
 # list colors
@@ -9,7 +11,7 @@ COLORS=`convert $GIF -unique-colors txt: | egrep -o "\#([A-F0-9]{6})FF"`;
 # start global svg
 echo '<?xml version="1.0" standalone="no"?>' > $SVG;
 echo '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">' >> $SVG;
-echo '<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="256.000000pt" height="256.000000pt" viewBox="0 0 256.000000 256.000000" preserveAspectRatio="xMidYMid meet">' >> $SVG;
+echo "<svg version=\"1.0\" xmlns=\"http://www.w3.org/2000/svg\" width=\"${W}pt\" height=\"${H}pt\" viewBox=\"0 0 $W $H\" preserveAspectRatio=\"xMidYMid meet\">" >> $SVG;
 for i in $COLORS; do
   COLOR=${i:1:-2};
   COLOR_GIF=${1/.png/_$COLOR.gif};
